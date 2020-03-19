79381de752f522323f2cc3b13f5c958f5e19c15f
kinmat <44319907+kinmat@users.noreply.github.com>
2020-03-19 23:57:18 +0100

    Update Karakter.java

--- Jégmező/src/szkeleton/Karakter.java
+++ Jégmező/src/szkeleton/Karakter.java
@@ -2,4 +2,62 @@ package szkeleton;
 
 public class Karakter {
 
+    int ho;
+    int munkak_szama;
+    bool vizben_van;
+    Jegtabla jegtabla;
+    Targy[] zseb;
+
+
+    public void takarit(Jegtabla j, int intensity) {
+
+    }
+
+    public void vizbe_esik () {
+
+    }
+
+    public void lep(Jegmezo jm) {
+
+    }
+
+    public bool menekit(Karakter k) {
+        return true;
+    }
+
+    public bool targy_felvetele() {
+        return true;
+    }
+
+    public bool tárgy_hozzadasa(Targy t) {
+        return true;
+    }
+
+    public bool kias(j: Jegtabla) {
+
+    }
+
+    public void sebzodik() {
+
+    }
+
+    public void ho_nov() {
+
+    }
+
+    public Jegtabla getJegtabla() {
+        return Jegtabla;
+    }
+
+    public void setJegtabla(Jegtabla j) {
+        jegtabla=j;
+    }
+
+    public bool van_munkaja() {
+        return true;
+    }
+
+    public void hasznal(Targy t, Jegtabla hol) {
+
+    }
 }
