cc8ff21dac694c12a5f6441576adfbf676708ce8
Nagy Ádám <nagy.addam@gmail.com>
2020-03-22 15:28:54 +0100

    Kötél

--- Jégmező/src/szkeleton/Kotel.java
+++ Jégmező/src/szkeleton/Kotel.java
@@ -7,6 +7,9 @@ public class Kotel extends Targy {
 	public void hasznaljak(Karakter karakter, Jegtabla hol) {
         System.out.println("------->   hasznaljak(Karakter karakter, Jegtabla hol)");
         ArrayList<Karakter> mentendo = hol.getKarakterek();
+        for (Karakter kar : mentendo) {
+        	karakter.menekit(kar);
+        }
 		System.out.println("<-------  hasznaljak(Karakter karakter, Jegtabla hol)");
 	}
 }
