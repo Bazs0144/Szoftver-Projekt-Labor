9cb9670abd5f52b6e34e610a309dba153b318d70
Nagy Ádám <nagy.addam@gmail.com>
2020-03-27 11:34:10 +0100

    Karakter kivéve a Tárgyból

--- Jégmező/src/szkeleton/Targy.java
+++ Jégmező/src/szkeleton/Targy.java
@@ -3,20 +3,10 @@ package szkeleton;
 public class Targy {
 	
 	private String nev;
-	private Karakter karakter;
+	
     public void hasznaljak(Karakter karakter, Jegtabla hol) {
 		Szkeleton.printFunction("------->  hasznaljak(Karakter, Jegtabla)", true, this);
 		Szkeleton.printFunction("<-------  hasznaljak(Karakter, Jegtabla)", false, this);
     	return;
-    }
-    public void setKarakter(Karakter kar) {
-		Szkeleton.printFunction("------->  setKarakter(Karakter)", true, this);
-    	karakter = kar;
-		Szkeleton.printFunction("<-------  setKarakter(Karakter)", false, this);
-    }
-    public Karakter getKarakter() {
-		Szkeleton.printFunction("------->  getKarakter()", true, this);
-		Szkeleton.printFunction("<-------  getKarakter()", false, this);
-     	return karakter;
     }
 }
