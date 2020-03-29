76b7a1d1214e3fd6914d4c71f99676392442168a
LizaSzabo <47565900+LizaSzabo@users.noreply.github.com>
2020-03-29 12:43:58 +0200

    kommentelve

--- Jégmező/src/szkeleton/Instabil.java
+++ Jégmező/src/szkeleton/Instabil.java
@@ -2,15 +2,30 @@ package szkeleton;
 
 import java.util.Scanner;
 
+
+/**
+ * Az instabil jégtábla implementálásáért felelõs osztály az Instabil, a Jegtabla leszármazottja.
+ *
+ */
 public class Instabil extends Jegtabla{
+	/**
+	 * Az Instabil osztály karakterek_szama adattagja tárolja az aktuálisan a jégtáblán álló karakterek számát.
+	 */
 	private int karakterek_szama;
-//	private int kapacitas;
-	
+
+	/**
+	 * Az Instabil osztály konstruktóra, csak a konstruktorhivás kiirásának céljából lett implementálva */
 	public Instabil() {
 		Szkeleton.printFunction("-------> Instabil()  *konstruktor*", true, this);
 		Szkeleton.printFunction("<------- Instabil()  *konstruktor*", false, this);
 	}
-	
+
+	/**
+	 * A Jegtabla osztályból öröklött ralepnek függvény felülirása:
+	 * Az aktiv karakter, amely egy szomszédos jégtábláról lép a jelenlegire, felkerül a jégtáblára (ennek nyilvántartásába).
+	 * A karakternek is beállitjuk az aktuális jégtábláját.
+	 * Ezután megviszgálja ha az instabil jégtáblán álló karakterek száma meghaladja-e a kapacitását. 
+	 * Ha igen, az insatbil jégtábla meghivja a felfordul függvényét.*/
 	@Override
 	public void ralepnek(Karakter k) {
 		Szkeleton.printFunction("------->ralepnek(Karakter k)", true, this);
@@ -26,9 +41,13 @@ public class Instabil extends Jegtabla{
 		Szkeleton.printFunction("<-------ralepnek(Karakter k)", false, this);
 	}
 	
+
+	/**
+	 * Amikor az instabil jégtábla felfordul, a rajta álló karakter(ek) a vizbe esnek. A karakter vizbe_esik függvénye hivódik meg. */
 	public void felfordul(Karakter k) {
 		Szkeleton.printFunction("------->felfordul(Karakter k)", true, this);
 		k.vizbe_esik();
 		Szkeleton.printFunction("<-------felfordul(Karakter k)", false, this);
 	}
 }
+
--- Jégmező/src/szkeleton/Luk.java
+++ Jégmező/src/szkeleton/Luk.java
@@ -1,12 +1,24 @@
 package szkeleton;
 
+/**
+ * Az luk implementálásáért felelõs osztály a Luk, a Jegtabla leszármazottja.
+ *
+ */
 public class Luk extends Jegtabla{
 
+	/**
+	 * A Luk osztály konstruktóra, csak a konstruktorhivás kiirásának céljából lett implementálva */
 	public Luk() {
 		Szkeleton.printFunction("-------> Luk()  *konstruktor*", true, this);
 		Szkeleton.printFunction("<------- Luk()  *konstruktor*", false, this);
 	}
 	
+	/**
+	 * A Jegtabla osztályból öröklött ralepnek függvény felülirása.
+	 * Az aktiv karakter, amely egy szomszédos jégtábláról lép a jelenlegire (lukra), felkerül a jégtáblára (ennek nyilvántartásába).
+	 * A karakternek is beállitjuk az aktuális jégtábláját.
+	 * A luk jégtábla kapacitása nulla, ezért a karakter aki rálép egybõl vizbe esik. 
+	 * Ez a karakter vizbe_esik függvényével valósul majd meg.*/
 	@Override
 	public void ralepnek(Karakter k) {
 		Szkeleton.printFunction("------->ralepnek(Karakter k)", true, this);
