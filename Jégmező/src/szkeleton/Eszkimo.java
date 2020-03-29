501d274d7f66d767f24dc9ae12b0bb5e02f8eaac
LovasTamas <61619137+LovasTamas@users.noreply.github.com>
2020-03-29 22:26:31 +0200

    Komment

--- Jégmező/src/szkeleton/Eszkimo.java
+++ Jégmező/src/szkeleton/Eszkimo.java
@@ -2,11 +2,18 @@ package szkeleton;
 
 public class Eszkimo extends Karakter {
 
+	/**
+	 * Az eszkimó osztály konstruktora, mely kiírás miatt lett implementálva.
+	 */
 	public Eszkimo() {
 		Szkeleton.printFunction("-------> Eszkimo() *konstruktor*", true, this);
 		Szkeleton.printFunction("<------- Eszkimo() *konstruktor*", false, this);
 	}
 
+	/**
+	 * Az eszkimó különleges képessége, amellyel iglut építhetünk a tartózkodási helyén.
+	 * @return: Ha sikerült az iglu építése igazzal tér vissza, egyébként hamissal.
+	 */
 	public boolean iglut_epit() {
 		Szkeleton.printFunction("------->  iglut_epit()", true, this);
 		
@@ -16,6 +23,7 @@ public class Eszkimo extends Karakter {
 				{
 					jegtabla.igluEpitese();
 					Szkeleton.printFunction("<--true-- iglut_epit()", false, this);
+					//this.munkak_szama--;
 					return true;
 				}
 		}
--- Jégmező/src/szkeleton/Sarkkutato.java
+++ Jégmező/src/szkeleton/Sarkkutato.java
@@ -2,16 +2,27 @@ package szkeleton;
 
 public class Sarkkutato extends Karakter {
 	
+	/**
+	 * Sarkkutato osztály konstruktora, mely kiírás miatt lett implementálva.
+	 */
 	public Sarkkutato() {
 		Szkeleton.printFunction("-------> Sarkkutato() *konstruktor*", true, this);
 		Szkeleton.printFunction("<------- Sarkkutato() *konstruktor*", false, this);
 	}
 	
+	
+	/**
+	 * A sarkkutató képessége, amellyel meg lehet tudni egy szomszédos mezõrõl, hogy hány embert bír el.
+	 * @param j: A jégtábla, amelynek szeretnénk megtudni a kapacitását.
+	 * @return kap: A jégtábla kapacitása, ha -1-el tér vissza, akkor nem szomszédos mezõt próbált megvizsgálni a karakter.
+	 */
 	public int megnez(Jegtabla j) {
 		Szkeleton.printFunction("------->  megnez(Jegtabla j)", true, this);
 		int kap;
-		if(this.jegtabla.szomszed_e(j))
+		if(this.jegtabla.szomszed_e(j)) {
 			kap = j.getKapacitas();
+			//this.munkak_szama--;
+			}
 		else
 			kap = -1;
 		Szkeleton.printFunction("<------- megnez(Jegtabla j)", false, this);
